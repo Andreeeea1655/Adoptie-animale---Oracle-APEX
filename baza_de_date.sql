b2da9ea0fe966d79b9fa2c74605ffe19d2351820
Andreea <166999031+Andreeeea1655@users.noreply.github.com>
2026-03-20 12:16:08 +0200

    Delete baza_de_date.sql

--- baza_de_date.sql
+++
@@ -1,9 +0,0 @@
---recunoastere sql, cod din docx
-drop table aTip cascade constraints;
-create Table aTip(
-  tip char (1) not null,
-  denumire varchar2 (10),
-  necesarAvizM char (2) check (necesarAvizM in ('da', 'nu')),
-
-  constraint tip_pk primary key (tip)
-  );
